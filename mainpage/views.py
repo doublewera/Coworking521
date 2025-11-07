72597f5e377c2c99f9573fd07fee3fa9063267de
Wera Barinova <alisawera@gmail.com>
2025-11-07 22:10:47 +0300

    Директива цикла for

--- mainpage/views.py
+++ mainpage/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'admin_today': 'Вася'  # Позже эти данные можно взять из БД
+        'all_admins': [
+            {
+                'name': 'Вася',  # Позже эти данные можно взять из БД
+                'age': 25,
+            },
+            {
+                'name': 'Ася',
+                'age': 32,
+            }
+        ]
     }
     return render(  # Функция "рендерит" шаблон - наполняет данными шаблон html страницы
         request,                 # так всегда
